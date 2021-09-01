13e3481907d1ed61b37251a705002e7619705284
Christopher Barker <cmbarker50@gmail.com>
2021-08-31 23:29:11 -0400

    Added tons of top-nav-bar html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopNavBarComponent } from '../app/header/top-nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavBarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/header/top-nav-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-top-nav-bar',
+    templateUrl: './top-nav-bar.component.html',
+    // styleUrls: ['./top-nav-bar.component.css']
+  })
+
+export class TopNavBarComponent{
+
+}
