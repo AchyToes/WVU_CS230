de24c446dc907044c86021c9098b93df1191935d
AchyToes <cmbarker50@gmail.com>
2021-09-06 22:22:35 -0400

    Finalized board component and added login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TopNavBarComponent } from '../app/header/top-nav-bar.component';
 import { SideNavBarComponent } from './sidenav/side-nav-bar.component';
+import { BoardComponent } from './board/board.component';
+import { LoginComponent } from './board/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavBarComponent,
-    SideNavBarComponent
+    SideNavBarComponent,
+    BoardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
--- src/app/header/top-nav-bar.component.ts
+++ src/app/header/top-nav-bar.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-top-nav-bar',
     templateUrl: './top-nav-bar.component.html',
-    // styleUrls: ['./top-nav-bar.component.css']
+    styleUrls: ['./top-nav-bar.component.css']
   })
 
 export class TopNavBarComponent{
