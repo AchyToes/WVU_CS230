7a99c7b8b164b7ca3545edb6d277ab2b0c57724c
AchyToes <cmbarker50@gmail.com>
2021-10-20 15:38:04 -0400

    Updated my website to get stock info from the backend

--- src/app/watchlist/watchlist.component.ts
+++ src/app/watchlist/watchlist.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { StockServiceService } from "src/services/stock-service.service";
 import { mock_stock } from "../mock-stock";
 import { Stock } from "../stock.model";
 
@@ -11,10 +12,29 @@ import { Stock } from "../stock.model";
 export class WatchlistComponent{
     
     stocks:Stock[] = [];
+    storageArray:Stock[] = [];
 
-    constructor() { 
-      for (var stock of mock_stock) {
+    constructor(private stockServiceService:StockServiceService) { 
+      this.getStocks();
+      for (var stock of this.storageArray) {
+        console.log(stock);
         this.stocks.push(new Stock(stock));
       }
     }
-}
+    getStocks()
+    {
+        this.stockServiceService.getStocks().subscribe((data: Stock[]) => {
+            console.log("i subscribed");
+            console.log(data);
+            let i = 0;
+            for (var point in data)
+            {
+                console.log(data[point]);
+                this.storageArray[i] = data[point];
+                i++;
+                this.stocks.push(new Stock(data[point]));
+            }
+        });
+    }
+
+  }
